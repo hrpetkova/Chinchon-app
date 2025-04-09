e20d5aea0f08ba919a6fe06a677ce6628b88fae4
Hristiyana Velichkova Petkova <hr.petkova@alumnos.upm.es>
2025-04-09 18:09:34 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -44,4 +44,10 @@ self.addEventListener('fetch', (event) => {
       return response || fetch(event.request);
     })
   );
+  self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
 });
