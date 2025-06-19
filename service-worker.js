ad1ad3cd37f72be64387001e6d154f0d35808251
Hristiyana Velichkova Petkova <hr.petkova@alumnos.upm.es>
2025-06-19 13:12:02 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chinchon-v2'; // cambia el número cuando actualices la app
+const CACHE_NAME = 'chinchon-v3'; // cambia el número cuando actualices la app
 
 const FILES_TO_CACHE = [
   './',
